b1c7f6f19e9402a7edccd213edbf19bedbe52bd4
Vatey11 <137879197+Vatey11@users.noreply.github.com>
2023-07-30 10:04:46 +0700

    update exercise-2

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,10 @@
 def index_power(numnbers, n):
-    ...
+    # check the length of the list.
+    # there are 2 conditions: 1. when the length list > n 2. when the length list < 2
+    # for condition 1: return list[n] ** n
+    # for condition 2: return -1
+    if len(numnbers) > n:
+        return numnbers[n] ** n
+    else:
+        return -1
+print(index_power([1, 2], 3))
