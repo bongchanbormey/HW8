82f437bcd65c47275cb056079a5e5f7bcc94a9dd
bongchanbormey <2023325bong@aupp.edu.kh>
2023-07-31 22:26:39 +0700

    Update exercise-1

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,15 @@
+# In a given list the last element should become the first one. 
+# An empty list or list with only one element should stay the same.
+# Example:
+# replace_last([2, 3, 4, 1]) == [1, 2, 3, 4]
+# replace_last([1, 2, 3, 4]) == [4, 1, 2, 3]
+# replace_last([1]) == [1]
+# replace_last([]) == []
+
 def replace_last(numbers):
-    ...
+    last_num = numbers[-1:]
+    exclude_last_num = numbers[:-1]
+    numbers = last_num + exclude_last_num
+    return numbers
+
+print(replace_last([]))
